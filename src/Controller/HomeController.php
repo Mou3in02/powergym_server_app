032af5e57804b699838c896f60dd84e52a9e226e
mou3in02 <mou3in02@gmail.com>
2025-06-28 13:47:36 +0100

    clean code

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class HomeController extends AbstractController
 {
