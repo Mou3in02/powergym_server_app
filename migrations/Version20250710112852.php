1659e527f3d44fc8b4a5d7ea7e07c28928310778
lahbib yassine <lahbibyassine2018@gmail.com>
2025-07-20 18:54:25 +0100

    fix base html

--- migrations/Version20250710112852.php
+++ migrations/Version20250710112852.php
@@ -21,7 +21,7 @@ final class Version20250710112852 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('
-            CREATE TABLE session_pers (
+            CREATE TABLE pers_session (
                 id SERIAL NOT NULL, 
                 id_client VARCHAR(100) DEFAULT NULL, 
                 first_name VARCHAR(255) NOT NULL, 
@@ -37,6 +37,6 @@ final class Version20250710112852 extends AbstractMigration
     public function down(Schema $schema): void
     {
 
-        $this->addSql('DROP TABLE session_pers');
+        $this->addSql('DROP TABLE pers_session');
     }
 }
