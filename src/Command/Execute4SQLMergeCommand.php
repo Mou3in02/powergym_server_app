2d85311ae0d016dc74e272da92a899c855e89411
Mou3in02 <mou3in02@gmail.com>
2025-08-28 23:25:33 +0100

    update acc person table

--- src/Command/Execute4SQLMergeCommand.php
+++ src/Command/Execute4SQLMergeCommand.php
@@ -3,9 +3,11 @@
 namespace App\Command;
 
 use App\Entity\FileExecution;
+use App\Factory\DatabaseConnectionFactory;
 use App\helpers\TimeFormatter;
 use App\Service\DataLoader;
 use DateTime;
+use Doctrine\DBAL\Connection;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Psr\Log\LoggerInterface;
@@ -21,14 +23,16 @@ use Symfony\Component\Finder\Finder;
 class Execute4SQLMergeCommand extends Command
 {
     public function __construct(
-        private readonly DataLoader             $dataLoader,
-        private readonly LoggerInterface        $logger,
-        private readonly Filesystem             $filesystem,
-        private readonly EntityManagerInterface $em,
-        private readonly string                 $targetDirectory
+        private readonly DataLoader                $dataLoader,
+        private readonly LoggerInterface           $logger,
+        private readonly Filesystem                $filesystem,
+        private readonly EntityManagerInterface    $em,
+        private readonly string                    $targetDirectory,
+        private readonly DatabaseConnectionFactory $databaseConnectionFactory,
     )
     {
         parent::__construct();
+        $this->tmpDB = $databaseConnectionFactory->getSecondConnection();
     }
 
     protected function configure()
@@ -59,39 +63,59 @@ class Execute4SQLMergeCommand extends Command
                 return Command::FAILURE;
             }
 
-            $io->info('Merging folder - ' . $timestamp . ' ...');
-            $files = $this->getAllExportedFiles($this->targetDirectory . '/' . $timestamp);
-            $now = new DateTime();
-            $fileExecution = (new FileExecution())
-                ->setFilename('TMP_DATABASE')
-                ->setType(FileExecution::TYPE_MERGE)
-                ->setSize(0)
-                ->setSizeDescription(0)
-                ->setCreatedAt($now)
-                ->setStartAt($startTime)
-                ->setIsDeleted(false);
+//            $io->info('Merging folder - ' . $timestamp . ' ...');
+//            $files = $this->getAllExportedFiles($this->targetDirectory . '/' . $timestamp);
+//            $now = new DateTime();
+//            $fileExecution = (new FileExecution())
+//                ->setFilename('TMP_DATABASE')
+//                ->setType(FileExecution::TYPE_MERGE)
+//                ->setSize(0)
+//                ->setSizeDescription(0)
+//                ->setCreatedAt($now)
+//                ->setStartAt($startTime)
+//                ->setIsDeleted(false);
 
-            foreach ($files as $file) {
-                $io->text("Executing SQL file: <fg=magenta>{$file['filename']}</> ...");
-                // Get database connection parameters from Doctrine
-                try {
-                    $result = $this->dataLoader->executePsql($file['path'], DataLoader::DATABASE_NAME, FileExecution::TYPE_MERGE);
-                    $executedFolder->setStatus(FileExecution::STATUS_SUCCESS);
-                    $fileExecution->setStatus(FileExecution::STATUS_SUCCESS);
-                } catch (Exception $e) {
-                    $io->error('Error executing SQL file: ' . $file['filename']);
-                    $this->logger->error($e->getMessage());
-                    $fileExecution->setStatus(FileExecution::STATUS_FAILED);
-                } finally {
-                    $endTime = microtime(true);
-                    $executionTime = $endTime - $startTime;
-                    $fileExecution->setEndAt($endTime)
-                        ->setExecutionTime($executionTime)
-                        ->setExecutionTimeDescription(TimeFormatter::formatShort($executionTime));
-                    $this->em->persist($fileExecution);
-                }
+//            foreach ($files as $file) {
+//                $io->text("Executing SQL file: <fg=magenta>{$file['filename']}</> ...");
+//                // Get database connection parameters from Doctrine
+//                try {
+//                    if ($file['filename'] === 'acc_person.sql') {
+//                        continue;
+//                    }
+//                    // $this->dataLoader->executePsql($file['path'], DataLoader::DATABASE_NAME, FileExecution::TYPE_MERGE);
+//                    $executedFolder->setStatus(FileExecution::STATUS_SUCCESS);
+//                    $fileExecution->setStatus(FileExecution::STATUS_SUCCESS);
+//                } catch (Exception $e) {
+//                    $io->error('Error executing SQL file: ' . $file['filename']);
+//                    $this->logger->error($e->getMessage());
+//                    $fileExecution->setStatus(FileExecution::STATUS_FAILED);
+//                } finally {
+//                    $endTime = microtime(true);
+//                    $executionTime = $endTime - $startTime;
+//                    $fileExecution->setEndAt($endTime)
+//                        ->setExecutionTime($executionTime)
+//                        ->setExecutionTimeDescription(TimeFormatter::formatShort($executionTime));
+//                    $this->em->persist($fileExecution);
+//                }
+//            }
+            // Get acc_person from a TMP database
+            if (!$this->tmpDB instanceof Connection) {
+                $io->error('Temporary database connection is not set !');
+                $this->logger->error("Temporary database connection is not set !");
+
+                return Command::FAILURE;
+            }
+            try {
+                $result = $this->getAccPersonRows();
+                dump($result);
+            } catch (Exception $e) {
+                $io->error('Error fetching acc_person table from TMP database');
+                $this->logger->error($e->getMessage());
+
+                return Command::FAILURE;
             }
         }
+        die;
 
         $this->em->flush();
         $io->success('Merge tmp data executed successfully');
@@ -113,6 +137,22 @@ class Execute4SQLMergeCommand extends Command
             ];
         }
         return $data;
+    }
+
+    /**
+     * @throws \Doctrine\DBAL\Exception
+     */
+    protected function getAccPersonRows(): array
+    {
+        $sql = "SELECT * FROM public.acc_person";
+        $result = $this->tmpDB->executeQuery($sql);
+
+        return $result->fetchAllAssociative();
+    }
+
+    protected function checkAccPersonRow(array $accPersonRow): void
+    {
+        $rowId = $accPersonRow['id'];
     }
 
 }
